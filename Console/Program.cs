6f02c5b06982a92ef582780efe3a9a9dc115245a
James Coliz <jcoliz@outlook.com>
2023-08-01 13:00:09 -0700

    feat(cli): Initial skeleton cli created and connected

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+
+try
+{
+    Console.WriteLine("BrewHub Digital Twins CLI");
+
+    string? adtInstanceUrl = Environment.GetEnvironmentVariable("TWINSURL");
+    HttpClient httpClient = new HttpClient();
+
+    if (adtInstanceUrl == null) 
+        throw new ApplicationException("ERROR: Application setting \"ADT_SERVICE_URL\" not set");
+
+    // Authenticate with Digital Twins
+    var cred = new DefaultAzureCredential();
+    var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+    Console.WriteLine($"OK. ADT service client connection created.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.GetType().Name} {ex.Message}");
+}
+
