12c94304b41bc32a6b1a6e343f0f460ec7a25be2
James Coliz <jcoliz@outlook.com>
2023-08-01 18:22:34 -0700

    build: dotnet new worker -o Replicator -n BrewHub.DigitalTwins.Replicator -f net7.0

---
+++ Replicator/Program.cs
@@ -0,0 +1,10 @@
+using BrewHub.DigitalTwins.Replicator;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
