29d9b4458fe535a46a9d0c402285a8e75a65b9d4
James Coliz <jcoliz@outlook.com>
2023-08-04 10:33:25 -0700

    feat: Add ClientSecretCredential for identity
    Fix AB#1681: Handle identity correctly

--- Replicator/TwinsClient/TwinsClient.cs
+++ Replicator/TwinsClient/TwinsClient.cs
@@ -1,4 +1,5 @@
 using Azure;
+using Azure.Core;
 using Azure.DigitalTwins.Core;
 using Azure.Identity;
 using Microsoft.Extensions.Options;
@@ -11,8 +12,14 @@ public class TwinsClient : ITwinsClient
 
     public class Options
     {
+        public enum CredentialType { Invalid = 0, DefaultAzureCredential, ClientSecretCredential };
+
         public const string Section = "DigitalTwins";
         public string? Url { get; set; }
+        public CredentialType Credential { get; set; } = CredentialType.DefaultAzureCredential;
+        public string? TenantId { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
     }
 
     public TwinsClient(IOptions<Options> options, ILoggerFactory logfact)
@@ -28,8 +35,23 @@ public class TwinsClient : ITwinsClient
                 throw new ApplicationException("Missing DigitalTwins.Url configuration");
 
             // Authenticate with Digital Twins
-            var cred = new DefaultAzureCredential();
-            _client = new DigitalTwinsClient(new Uri(_options.Url), cred);
+            TokenCredential? credential = null;
+
+            if (_options.Credential == Options.CredentialType.DefaultAzureCredential)
+            {
+                credential = new DefaultAzureCredential();
+            }
+            else if (_options.Credential == Options.CredentialType.ClientSecretCredential)
+            {
+                credential = new ClientSecretCredential(_options.TenantId, _options.ClientId, _options.ClientSecret);
+            }
+            else
+                throw new NotImplementedException($"Crendential type {_options.Credential} is not implemented");
+
+            if (credential is null)
+                throw new ApplicationException("Missing DigitalTwins credential type");
+
+            _client = new DigitalTwinsClient(new Uri(_options.Url), credential);
 
             _logger.LogInformation("Created client OK on {url}", _options.Url);
         }
